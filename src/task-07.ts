2194f27a67dc18a4b105b44a5c9ab924b72c79b1
Inna-Vinnyk <vinnik_inna@ukr.net>
2026-01-10 16:04:09 +0100

    task-07, task-08, task-09

--- src/task-07.ts
+++ src/task-07.ts
@@ -1,4 +1,14 @@
-const users = [
+interface User {
+  name: string;
+  age: number;
+}
+
+const users: User[] = [
   { name: "Alice", age: 30 },
   { name: "Bob", age: 25 },
 ];
+
+users.push({ name: "Sofia", age: 28 });
+console.log(users);
+
+users.push({ name: "Henry"}); // This line should cause a TypeScript error
